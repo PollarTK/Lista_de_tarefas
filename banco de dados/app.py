a80f1122f52252d868d8a2a593ff8d2c6e4276a3
LucaFreire <lucasrodifreire@gmail.com>
2025-03-27 17:12:25 -0300

    Estilo da pagina inicial

--- banco de dados/app.py
+++ banco de dados/app.py
@@ -1,4 +1,6 @@
-from flask import Flask, render_template  # importando a biblioteca Flask
+from flask import Flask, render_template, request, redirect, url_for, flash, session  # importando a biblioteca Flask
+import database
+import sqlite3
 
 app = Flask(__name__) # criando um objeto do flask chamado app
 
@@ -13,6 +15,17 @@ def login():
 @app.route('/cadastro')
 def cadastro():
     return render_template('cadastro.html')
+
+'''@app.route('/cadastrar', methods=['POST'])
+def cadastrar():
+    usuario = request.form['usuario']
+    email = request.form['email']
+    senha = request.form['senha']
+
+    if database.criar_usuario(email, usuario, senha):
+        return "Usuário criado com sucesso"
+    else:
+        return "Isso não funcionou"   '''
 
 if __name__ == '__main__':
     app.run(debug=True)
